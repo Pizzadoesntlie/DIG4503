716e968b00ee924b944b959d1a2623305553a50c
Pizzadoesntlie <32754723+Pizzadoesntlie@users.noreply.github.com>
2020-02-01 10:16:32 -0500

    lab 3 commit

---
+++ lab3/index.js
@@ -0,0 +1,13 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+App.use("/public", Express.static("public"));
+
+App.get("/", function(req, res) {
+    res.send("Hello World!");
+});
+
+App.listen(port, function() {
+    console.log("Server is running!");
+});
