5f9c59c95fb51ff66aafc4a1767d06cba7c3b2a8
Pizzadoesntlie <32754723+Pizzadoesntlie@users.noreply.github.com>
2020-02-08 23:35:04 -0500

    lab 4 commit

---
+++ lab4/index.js
@@ -0,0 +1,43 @@
+const Express = require("Express");
+const App = Express();
+const chalk = require("chalk");
+const pokemons = require('json-pokemon');
+const  port = 80;
+
+let result = {"error": "id/name not found"};
+
+App.get("/id/:id", (req,res) => {
+    pokemons.forEach((value) => {
+        if(value.id == req.params.id) {
+            res.send(value);
+        }
+    });
+
+    if(result.error) {
+        console.log(chalk.red(req.params.id));
+    }else {
+        console.log(chalk.green(req.path));
+    }
+   
+    res.send(result);
+});
+
+App.get("/name/:name", (req,res) => {
+    pokemons.forEach((value) => {
+        if(value.name == req.params.name) {
+            res.send(value);
+        }
+    });
+
+    if(result.error) {
+        console.log(chalk.red(req.params.name));
+    }else {
+        console.log(chalk.green(req.path));
+    }
+   
+    res.send(result);
+});
+
+App.listen(port, () => {
+    console.log("Server running!");
+});
