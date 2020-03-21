baa0e6ca79422ffae41bae41b89c0ced83834538
Pizzadoesntlie <32754723+Pizzadoesntlie@users.noreply.github.com>
2020-03-21 16:10:03 -0400

    lab8 commit

---
+++ lab8/pages/index.js
@@ -0,0 +1,15 @@
+import NameSearch from '../components/NameSearch/';
+import IdSearch from '../components/IdSearch/';
+
+const Home = () => {
+    return (
+        <div>
+        <NameSearch />
+        <IdSearch />
+        <h2>Reporting</h2>
+  <div id="reportingArea"></div>
+        </div>
+    )
+}
+
+export default Home;
